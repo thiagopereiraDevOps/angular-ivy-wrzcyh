35e403675b0c2ad0a6b2b897668c259f0804e28f
thiagopereiraDevOps <pereira3720@yahoo.com>
2022-07-08 14:08:58 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './components/first-component.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, FirstComponentComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
