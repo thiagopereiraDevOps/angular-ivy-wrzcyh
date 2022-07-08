55ca3205797cac8a2ab4143542e5346780005d1c
thiagopereiraDevOps <pereira3720@yahoo.com>
2022-07-08 14:13:42 -0300

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
 import { FirstComponentComponent } from './components/first-component.component';
-
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, FirstComponentComponent ],
+  declarations: [ AppComponent, HelloComponent, FirstComponentComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
